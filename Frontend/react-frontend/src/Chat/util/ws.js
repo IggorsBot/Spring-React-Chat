8560d6f5a5fd053d37ec2991f022470ac7f2f634
Igor <IggorsBot@gmail.com>
2020-08-04 12:26:13 +0300

    add WebSocket

---
+++ Frontend/react-frontend/src/Chat/util/ws.js
@@ -0,0 +1,33 @@
+// Third-Party
+import SockJS from 'sockjs-client'
+import {Stomp} from '@stomp/stompjs'
+
+
+var stompClient = null
+const handlers = []
+
+export function connect() {
+    const socket = SockJS('http://localhost:8080/chat-websocket')
+    stompClient = Stomp.over(socket)
+    stompClient.connect({}, frame => {
+        console.log("Connected " + frame)
+        stompClient.subscribe('/topic/active', message => {
+            handlers.forEach(handler => handler(JSON.parse(message.body)))
+        })
+    })
+}
+
+export function addHandler(handler) {
+    handlers.push(handler)
+}
+
+export function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect()
+    }
+    console.log("Disconnected")
+}
+
+export function sendMessage(message) {
+    stompClient.send("/app/changeMessage", {}, JSON.stringify(message))
+}
