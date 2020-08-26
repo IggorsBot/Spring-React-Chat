a18046d7402e470c005e8492479d2e3ad1997f09
IggorsBot <IggorsBot@gmail.com>
2020-08-26 12:02:13 +0300

    Refactoring Component FormForNewChat

--- Frontend/react-frontend/src/Chat/FormForNewChat/FormForNewChat.js
+++ Frontend/react-frontend/src/Chat/FormForNewChat/FormForNewChat.js
@@ -10,38 +10,35 @@ import {getUsernames, addHandler, newChat} from './../util/ws'
 // Third-party
 import Modal from 'react-modal';
 
-// CSS
-import { newChatForm, userInFoundedUsersList, foundedUsersList } from './FormForNewChatCSS';
-
 
 Modal.setAppElement("#root")
 
 function FormForNewChat() {
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [foundedUsers, setFoundedUsers] = useState({
+    const [foundedUsersList, setFoundedUsersList] = useState({
         activeStateUsers: null,
         users: []
     });
-    const [searchUser, setSearchUser] = useState("");
+    const [usernameForSearch, setUsernameForSearch] = useState("");
     const [userForNewChat, setUserForNewChat] = useState("");
 
     React.useEffect(() => {
         addHandler("UserListHandler", (data) => {
-            setFoundedUsers({...foundedUsers, users: data, activeStateUsers: null})
+            setFoundedUsersList({...foundedUsersList, users: data, activeStateUsers: null})
         });
     }, []);
 
     React.useEffect(() => {
-        getUsernames({username: searchUser})
-    }, [searchUser]);
+        getUsernames({username: usernameForSearch})
+    }, [usernameForSearch]);
 
     function handleSearchUser(evt) {
         if (evt.target.value.length === 0) {
-            setFoundedUsers({...foundedUsers, users: [], activeStateUsers: null})
+            setFoundedUsersList({...foundedUsersList, users: [], activeStateUsers: null})
             return
         }
-        setSearchUser(evt.target.value)
+        setUsernameForSearch(evt.target.value)
     }
 
     function createNewChat () {
@@ -52,17 +49,18 @@ function FormForNewChat() {
     }
 
     function closeModal () {
-        setFoundedUsers({...foundedUsers, users: [], activeStateUsers: null})
+        setFoundedUsersList({...foundedUsersList, users: [], activeStateUsers: null})
         setModalIsOpen(false)
     }
 
     function toggleActive(index) {
-        setFoundedUsers({...foundedUsers, activeStateUsers: foundedUsers.users[index]})
+        setFoundedUsersList({...foundedUsersList, activeStateUsers: foundedUsersList.users[index]})
+
     }
 
     function toggleActiveStyles(index) {
-        if (foundedUsers.users[index] === foundedUsers.activeStateUsers) {
-            return "active";
+        if (foundedUsersList.users[index] === foundedUsersList.activeStateUsers) {
+            return "user-active";
         } else {
             return ""
         }
@@ -88,20 +86,22 @@ function FormForNewChat() {
                 </div>
 
                 <div css={foundedUsersList}>
-                {foundedUsers.users.slice(0,3).map((user, index) => {
-                    return (
-                        <div css={userInFoundedUsersList} className={toggleActiveStyles(index)} key={index} onClick={() => {setUserForNewChat(user); toggleActive(index)}}>
-                            <img />
-                            <p>{user}</p>
-                        </div>
-                    )
-                })}
+                    {foundedUsersList.users.slice(0,3).map((user, index) => {
+                        return (
+                            <div css={userInFoundedUsersList}
+                                 className={toggleActiveStyles(index)}
+                                 key={index}
+                                 onClick={() => {setUserForNewChat(user); toggleActive(index)}}>
+                                <img />
+                                <p>{user}</p>
+                            </div>
+                        )
+                    })}
                 </div>
-
 
                 <button
                     type="button"
-                    className={userForNewChat.length > 0 ? "button-new-chat" : "button-new-chat-deactivate"}
+                    className={userForNewChat.length > 0 ? "button button-activate" : "button button-deactivate"}
                     onClick={() => {createNewChat(); closeModal()}}
                 >
                     Create
@@ -109,13 +109,11 @@ function FormForNewChat() {
 
                 <button
                     type="button"
-                    className="button-cancel"
+                    className="button button-cancel"
                     onClick={() => closeModal()}
                 >
                     Exit
                 </button>
-
-
 
             </Modal>
         </Fragment>
@@ -123,3 +121,104 @@ function FormForNewChat() {
 }
 
 export default FormForNewChat;
+
+
+const newChatForm = () =>
+    css`
+    position: absolute;
+
+    top: 20%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+
+    left: 50%;
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+
+    min-width: 500px;
+    max-width: 500px;
+
+    min-height: 400px;
+    max-height: 400px;
+
+    background-color: white;
+    overflow: hidden;
+    outline:0;
+    border-style: solid;
+    border-radius: 10px;
+    border-color: #eee;
+
+    padding: 50px 80px;
+    text-align: center;
+
+    h1 {
+        align-items: center;
+        color: #0048AA;
+        font-weight: bold;
+        font-size: 2.0rem;
+        padding: 0 20px;
+    }
+
+    .button {
+        position: absolute;
+        display: block;
+        width: 80px;
+        height: 40px;
+        border: none;
+        background-size: 200%;
+        color: #fff;
+        outline: none;
+        cursor: pointer;
+        transition: .5s;
+        }
+    }
+
+    .button-activate {
+        right:    80px;
+        bottom:   50px;
+        background: #0048AA;
+    }
+
+    .button-deactivate {
+        right:    80px;
+        bottom:   50px;
+        display: block;
+        background: #5082c7;
+        }
+    }
+
+    .button-cancel {
+        bottom:   50px;
+        background: #0048AA;
+    }
+
+    .user-active {
+        background-color: #bbdefb;
+    }
+    `;
+
+
+const userInFoundedUsersList = () =>
+    css`
+    font-size: 1.3rem;
+    padding: 0px 10px 0px 15px;
+    font-weight: bold;
+    padding: 10px 5px;
+    display: flex;
+    align-items:center;
+
+    p {
+        padding-left: 20px; */
+    }
+
+    :hover {
+        cursor: pointer;
+        background-color: #bbdefb;
+    }
+
+    img {
+        height: 40px;
+        width: 40px;
+        border-radius: 100%;
+    }
+    `;
